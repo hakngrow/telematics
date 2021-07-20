53aa0fce689769d7ffe6e81ccb3be427869ba9e8
unknown <hakngrow@gmail.com>
2021-07-21 06:37:50 +0800

    Resolved mongoose Decimal128 issue

--- models/performance.js
+++ models/performance.js
@@ -1,5 +1,6 @@
 // models/peformance.js
 
+const { json } = require('body-parser');
 const mongoose = require('mongoose')
 
 const PerformanceSchema = new mongoose.Schema({
@@ -18,31 +19,46 @@ const PerformanceSchema = new mongoose.Schema({
   distance: {
     type: mongoose.Decimal128,
     required: true,
+    get: getDecimal
   },
   duration: {
-    type: mongoose.Decimal128,
+    type: Number,
     required: true,
   },
   speeding: {
     type: mongoose.Decimal128,
     required: true,
+    get: getDecimal
   },
   distraction: {
     type: mongoose.Decimal128,
     required: true,
+    get: getDecimal
   },
   braking: {
     type: mongoose.Decimal128,
     required: true,
+    get: getDecimal
   },
   cornering: {
     type: mongoose.Decimal128,
     required: true,
+    get: getDecimal
   },
   updated_date: {
     type: Date,
     default: Date.now,
-  },
+  }
 })
+
+PerformanceSchema.set('toObject', { getters: true });
+PerformanceSchema.set('toJSON', { getters: true });
+
+function getDecimal(value) {
+  if (typeof value !== 'undefined') {
+     return parseFloat(value.toString());
+  }
+  return value;
+}
 
 module.exports = Performance = mongoose.model('performance', PerformanceSchema)
